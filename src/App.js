9d51668ec7b3134d42f103ad26c2482c89c5b01b
mehulkchaudhari <chaudharimehul2001@gmail.com>
2022-04-18 18:38:19 +0530

    initial extension setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './Rakshak-logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="rakshak-logo" />
+        <img src="https://img.icons8.com/color/48/000000/user.png" alt="user-icon" className='user-icon'/>
       </header>
     </div>
   );
