e0cc1ed9ca9d0039d7007c3580f474a70a68d701
Tejas Mandre <tejasmandre0306@gmail.com>
2022-05-09 17:16:02 +0530

    added bug fixes

--- core/accounts/serializers.py
+++ core/accounts/serializers.py
@@ -5,5 +5,5 @@ class UserSerializer(serializers.Serializer):
 
     email = serializers.EmailField(max_length=254)
     name = serializers.CharField(max_length=254)
-    protection_level = serializers.CharFiedl()
+    protection_level = serializers.CharField()
     last_login = serializers.DateTimeField()
