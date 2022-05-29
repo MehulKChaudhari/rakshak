927282be2d9734a1725f4762c1dfb4f80b122ef9
mehulkchaudhari <chaudharimehul2001@gmail.com>
2022-05-29 16:33:53 +0530

    added: wrapper upon hate speech with message

--- src/contentScript.js
+++ src/contentScript.js
@@ -6,8 +6,14 @@ chrome.storage.local.get("token", function (items) {
   }
 });
 
+/**logic to find true values in server response */
+function getTrueValues(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+
+/** Event to scrap and send data to server */
+
 window.addEventListener("click", (event) => {
-  const targetNode = event.target;
   console.log("cleickedede");
   console.log("href", window.location);
   let data;
@@ -16,11 +22,10 @@ window.addEventListener("click", (event) => {
   } else if (window.location.host === "twitter.com") {
     data = document.querySelectorAll('[data-testid="tweetText"]');
   } else if (window.location.host === "www.youtube.com") {
-    data = document.querySelectorAll("#content-text");
+    data = document.querySelectorAll("#content");
   }
 
   data.forEach((element) => {
-    console.log(element.innerText);
     fetch("http://localhost:8000/api/parse-string-social/", {
       method: "POST",
       body: JSON.stringify(element.innerText),
@@ -36,10 +41,40 @@ window.addEventListener("click", (event) => {
             data: data,
             status: response.status,
           }))
+          //** logic to hide and show messages to user based server response */
           .then((res) => {
             if (Object.values(res.data).indexOf(true) > -1) {
-              element.style.filter = "blur(8px)";
-              element.style.backgroundColor = "red";
+              /** creating wrapper  */
+              const text = document.createElement("p");
+
+              /**Logic to add blur effect to backgorund div */
+
+              if (window.location.host === "www.instagram.com") {
+                const child = element.children;
+                child[0].style.cssText =
+                  "filter:blur(8px);background-color :red; padding:10px";
+                element.appendChild(text);
+              } else if (window.location.host === "twitter.com") {
+                const parent = element.parentNode;
+                parent.appendChild(text);
+                element.style.cssText =
+                  "filter:blur(8px);background-color :red; padding:10px";
+              } else {
+                element.style.cssText =
+                  "filter:blur(8px);background-color :red; padding:10px";
+                element.appendChild(text);
+              }
+
+              /** adding css to to created wrapper  */
+              text.style.cssText =
+                "display: block; position: absolute; color:black; width:inherit; z-index:10; border-radius: 5px; padding:2px";
+              text.style.color = "black";
+              const serverData = Object.keys(res.data).find(
+                (key) => res.data[key] === true
+              );
+              console.log("server true", serverData);
+              /**adding text to created wrapper */
+              text.innerText = "This content has hate language";
             }
           })
       )
